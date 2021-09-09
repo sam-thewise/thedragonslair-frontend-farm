792349cafb6643ae0d6f05d159c2deaad753e1d6
sam-thewise <caliginosity.insanity@gmail.com>
2021-09-09 14:45:49 +1200

    fix usdt.e

--- src/utils/callHelpers.ts
+++ src/utils/callHelpers.ts
@@ -8,8 +8,14 @@ export const approve = async (lpContract, masterChefContract, account) => {
 }
 
 export const stake = async (masterChefContract, pid, amount, account) => {
+  let decimals = 18
+
+  if( pid === 5){
+    decimals = 6;
+  }
+
   return masterChefContract.methods
-    .deposit(pid, new BigNumber(amount).times(new BigNumber(10).pow(18)).toString())
+    .deposit(pid, new BigNumber(amount).times(new BigNumber(10).pow(decimals)).toString())
     .send({ from: account })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
@@ -35,8 +41,15 @@ export const sousStakeBnb = async (sousChefContract, amount, account) => {
 }
 
 export const unstake = async (masterChefContract, pid, amount, account) => {
+  let decimals = 18;
+
+  if( pid === 5 )
+  {
+    decimals = 6
+  }
+  
   return masterChefContract.methods
-    .withdraw(pid, new BigNumber(amount).times(new BigNumber(10).pow(18)).toString())
+    .withdraw(pid, new BigNumber(amount).times(new BigNumber(10).pow(decimals)).toString())
     .send({ from: account })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
